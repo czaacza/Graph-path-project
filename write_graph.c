df644b8e5ec44807688089576a60a64565766611
Pawel <jawelg3@gmail.com>
2022-03-27 04:43:27 +0200

    generator zapisuje graf do linked listy z krawędziami

--- write_graph.c
+++ write_graph.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct edges {	// lista krawędzi 
+	int start;	// wierzchołek początkowy
+	int end;	// w. końcowy
+	double  value;	// waga
+	struct edges *next;
+} *ptr;
+
+ptr add_edge(ptr graph, int start, int end, double value) {
+	// dodaje krawędź na początek linked listy
+	ptr nw = malloc(sizeof nw);
+
+	nw->next = graph;
+	nw->start = start;
+	nw->end = end;
+	nw->value = value;
+
+	return nw;
+
+}
+
 int main(int argc, char **argv)
 {
 	int i, j;
@@ -9,6 +29,46 @@ int main(int argc, char **argv)
 	int c = 4; // liczba kolumn
 	int r = 3; // liczba wierdszy
 
+	ptr graph = NULL;
+
+	for (i = 0; i < c * r - 1; i++) {
+		if (i >= (c - 1) * r)
+		{ // sprawdza czy wierzchołek jest w ostatnim wierszu
+			graph = add_edge(graph, i, i + 1, 5);
+			graph = add_edge(graph, i + 1, i, 5);	// zamiana indeksów, żeby krawędź była w obie strony
+		} else if (i % c == c - 1)
+		{ // sprawdza czy wierzchołek jest  w ostatniej kolumnie
+			graph = add_edge(graph, i, i + c, 5);
+			graph = add_edge(graph, i + c, i, 5);
+		}
+		else
+		{ // to się dzieje dla pozostałych wierzchołków
+			graph = add_edge(graph, i, i + 1, 5);
+			graph = add_edge(graph, i + 1, i, 5);
+			graph = add_edge(graph, i, i + c, 5);
+			graph = add_edge(graph, i + c, i, 5);
+		}
+	}
+
+	// wypisywanie do pliku
+	fprintf(out, "%d %d", r, c);
+	for (i = 0; i < c * r; i++) {	// dla każdego wierzchołka
+		fprintf(out, "\n");
+		ptr tmp = graph;
+		while(tmp != NULL) {	// przechodzi po liście krawędzi
+			if(tmp->start == i) {	// i szuka krawędzi, których początkiem jest nasz wierzchołek
+				fprintf(out, "%d :waga ", tmp->end);
+			}
+			tmp = tmp->next;
+		}
+	}
+
+	return 0;
+}
+
+
+/*
+// wersja z tablicą
 	int **graph = malloc(c * r * sizeof *graph);
 	for (i = 0; i < r * c; i++)
 		graph[i] = malloc(c * r * sizeof graph);
@@ -46,6 +106,4 @@ int main(int argc, char **argv)
 				fprintf(out, "%d :waga ", j);
 		}
 	}
-
-	return 0;
-}
+*/
