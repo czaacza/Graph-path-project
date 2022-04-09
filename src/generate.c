c0f870f9780b2753fb2e96193f0d328b385b4ceb
Pawel <jawelg3@gmail.com>
2022-04-09 23:55:18 +0200

    added chance

--- src/generate.c
+++ src/generate.c
@@ -21,24 +21,36 @@ int genGraph(graph_t graph, double chance, double minW, double maxW)
 	{
 		if (i >= numOfColumns * (numOfRows - 1))
 		{ // sprawdza czy wierzchołek jest w ostatnim wierszu
-			weight = randomInRange(minW, maxW);
-			graph->values[i][1] = weight;
-			graph->values[i + 1][0] = weight; // zamiana indeksów, żeby krawędź była w obie strony
+			if (chance < randomInRange(0, 100))
+			{
+				weight = randomInRange(minW, maxW);
+				graph->values[i][1] = weight;
+				graph->values[i + 1][0] = weight; // zamiana indeksów, żeby krawędź była w obie strony
+			}
 		}
 		else if (i % numOfColumns == numOfColumns - 1)
 		{ // sprawdza czy wierzchołek jest  w ostatniej kolumnie
-			weight = randomInRange(minW, maxW);
-			graph->values[i][2] = weight;
-			graph->values[i + numOfColumns][3] = weight;
+			if (chance < randomInRange(0, 100))
+			{
+				weight = randomInRange(minW, maxW);
+				graph->values[i][2] = weight;
+				graph->values[i + numOfColumns][3] = weight;
+			}
 		}
 		else
 		{ // to się dzieje dla pozostałych wierzchołków
-			weight = randomInRange(minW, maxW);
-			graph->values[i][1] = weight;
-			graph->values[i + 1][0] = weight;
-			weight = randomInRange(minW, maxW);
-			graph->values[i][2] = weight;
-			graph->values[i + numOfColumns][3] = weight;
+			if (chance < randomInRange(0, 100))
+			{
+				weight = randomInRange(minW, maxW);
+				graph->values[i][1] = weight;
+				graph->values[i + 1][0] = weight;
+			}
+			if (chance < randomInRange(0, 100))
+			{
+				weight = randomInRange(minW, maxW);
+				graph->values[i][2] = weight;
+				graph->values[i + numOfColumns][3] = weight;
+			}
 		}
 	}
 }
